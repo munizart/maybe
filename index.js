38ec5370d8c3950bb9978fa6dc969cf88775a539
Artur Muniz <munizart_dev@hotmail.com>
2018-10-11 15:46:40 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+function Just (a) {
+  function toString () {
+    return `Just(${a})`
+  }
+  return {
+    map (trasnform) {
+      return Just(trasnform(a))
+    },
+    chain (trasnform) {
+      return trasnform(a)
+    },
+    fold(transformNothing, transformJust) {
+      return transformJust(a)
+    },
+    inspect: toString,
+    toString
+  }
+}
+
+function Nothing () {
+  function toString () {
+    return `Nothing()`
+  }
+  return {
+    map (trasnform) {
+      return Maybe.Nothing()
+    },
+    chain (trasnform) {
+      return Maybe.Nothing()
+    },
+    fold(transformNothing, transformJust) {
+      return transformNothing()
+    },
+    inspect: toString,
+    toString
+  }
+}
+const _Nothing = Nothing()
+Object.freeze(_Nothing)
+
+module.exposts = {
+  of(x) {
+    return Maybe.Just(x)
+  },
+  Nothing: () => _Nothing,
+  Just
+}
